d29fccb3c5dfd1a071219924e2c2b993078a0c35
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-09-19 16:47:01 -0500

    all

---
+++ scripts/0_cron_etl.bash
@@ -0,0 +1,7 @@
+wget -O - https://raw.githubusercontent.com/mmvvff/henry_pg_anlytcs/main/scripts/1_setup.bash | bash
+
+# crons jobs para ETL
+cd ~/1_etl
+wget -O - https://raw.githubusercontent.com/mmvvff/henry_pg_anlytcs/main/scripts/2_extract_datoscrudos.bash | bash
+wget -O - https://raw.githubusercontent.com/mmvvff/henry_pg_anlytcs/main/scripts/3_transformar_v1.py | python3
+gcloud storage cp ~/1_etl/3_output --recursive gs://1_etl_output/
--- scripts/0b_cron_etl.bash
+++
@@ -1,5 +0,0 @@
-wget -O https://raw.githubusercontent.com/mmvvff/henry_pg_anlytcs/main/scripts/0a_setup.bash?token=GHSAT0AAAAAACF3AM2LJFUQT4SHH332PLTAZIKCLNQ | bash
-cd ~/1_etl
-wget -O https://raw.githubusercontent.com/mmvvff/henry_pg_anlytcs/main/scripts/1_extract_datoscrudos.bash?token=GHSAT0AAAAAACF3AM2LAZXTNAOPNCFA4NSMZIKCDNA | bash
-wget -O https://raw.githubusercontent.com/mmvvff/henry_pg_anlytcs/main/scripts/2_transformar_v1.py?token=GHSAT0AAAAAACF3AM2KIB5T5GJ4OMSOSPBKZIKCEIA | python3
-gcloud storage cp ~/1_etl/3_output --recursive gs://1_etl_output/

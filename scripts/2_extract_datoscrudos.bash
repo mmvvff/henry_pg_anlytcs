1084dab5b1fba9c8e16799de9a0235aba838a277
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-09-19 16:48:33 -0500

    Update 2_extract_datoscrudos.bash

--- scripts/2_extract_datoscrudos.bash
+++ scripts/2_extract_datoscrudos.bash
@@ -2,7 +2,7 @@ cd ~/1_etl/1_data/0_external
 sudo gdown --folder https://drive.google.com/drive/folders/1TI-SsMnZsNP6t930olEEWbBQdo_yuIZF
 sudo mkdir google_maps && cd google_maps
 sudo mkdir reviews_estados metadata_sitios && cd reviews_estados
-sudo gdown --folder https://drive.google.com/drive/folders/19QNXr_BcqekFNFNYlKd0kcTXJ0Zg7lI6
-cd .. && cd metadata_sitios
-sudo gdown --folder https://drive.google.com/drive/folders/1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z
+#sudo gdown --folder https://drive.google.com/drive/folders/19QNXr_BcqekFNFNYlKd0kcTXJ0Zg7lI6
+#cd .. && cd metadata_sitios
+#sudo gdown --folder https://drive.google.com/drive/folders/1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z
 cd ~
